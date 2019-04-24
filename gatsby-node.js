2c6dcbfa23a817ca606cba5fe58a47be7279d995
Carl Vitullo <carl@stellar.org>
2019-04-24 12:00:23 -0400

    chore: Remove unused variables

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,7 +27,7 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
         }
 
         const posts = result.data.allContentfulBlogPost.edges
-        posts.forEach((post, index) => {
+        posts.forEach((post) => {
           createPage({
             path: `/blog/${post.node.slug}/`,
             component: blogPost,
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import Link from 'gatsby-link'
-import base from './base.css'
+import './base.css'
 import Container from '../components/container'
 import Navigation from '../components/navigation'
 
 class Template extends React.Component {
   render() {
-    const { location, children } = this.props
-    let header
-
-    let rootPath = `/`
-    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-      rootPath = __PATH_PREFIX__ + `/`
-    }
+    const { children } = this.props
 
     return (
       <Container>
