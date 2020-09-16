da80fc6d27ccd64361249fc194f96b1fae82e03a
Aldi Cita Putra <aldi.putra@photoninfotech.net>
2020-09-16 13:57:07 +0700

    remove navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,13 +4,5 @@ import styles from './navigation.module.css'
 
 export default () => (
   <nav role="navigation">
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
-      </li>
-    </ul>
   </nav>
 )
