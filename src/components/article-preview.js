fb50b6670a7a66968132df5b1330f9cd664cc3be
Armando Vasquez <ar.xa.vasquez@gmail.com>
2020-12-17 16:01:44 -0600

    asdf

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -6,7 +6,7 @@ import styles from './article-preview.module.css'
 
 export default ({ article }) => (
   <div className={styles.preview}>
-    <Img alt="" fluid={article.heroImage.fluid} />
+    {article.heroImage.fluid && (<Img alt="" fluid={article.heroImage.fluid} />)}
     <h3 className={styles.previewTitle}>
       <Link to={`/blog/${article.slug}`}>{article.title}</Link>
     </h3>
