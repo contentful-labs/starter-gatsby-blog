5c9007b4a9d31bd3e3ac799f741a429f2b180df7
Florian Kissling <florian.kissling@gmail.com>
2021-07-26 01:01:56 +0200

    🧹

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,11 @@
 import React from 'react'
 
 import Container from "./container"
+import * as styles from "./footer.module.css"
 
 const Footer = () => (
   <Container>
-    <div style={{
-      borderTop: '1px solid var(--border)',
-      fontSize: 'var(--text-sm)',
-      marginTop: 'var(--space-2xl)',
-      padding: 'var(--space-2xl) 0',
-      textAlign: 'right',
-    }}>
+    <div className={styles.container}>
       Built with <a href="https://contentful.com/">Contentful</a> and <a href="https://gatsbyjs.com">Gatsby</a> &middot; <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
     </div>
   </Container>
