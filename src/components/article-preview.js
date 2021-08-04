d36c5be40b16fdf607d21d83975c5c101e3933ef
Florian Kissling <florian.kissling@gmail.com>
2021-08-04 18:13:43 +0200

    Guard against posts being falsy

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -6,30 +6,35 @@ import Container from './container'
 import Tags from './tags'
 import * as styles from './article-preview.module.css'
 
-const ArticlePreview = ({ posts }) => (
-  <Container>
-    <ul className={styles.articleList}>
-      {posts.map((post) => {
-        return (
-          <li key={post.slug}>
-            <Link to={`/blog/${post.slug}`} className={styles.link}>
-              <GatsbyImage alt="" image={post.heroImage.gatsbyImageData} />
-              <h2 className={styles.title}>{post.title}</h2>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: post.description.childMarkdownRemark.html,
-                }}
-              />
-              <div className={styles.meta}>
-                <small className="meta">{post.publishDate}</small>
-                <Tags tags={post.tags} />
-              </div>
-            </Link>
-          </li>
-        )
-      })}
-    </ul>
-  </Container>
-)
+const ArticlePreview = ({ posts }) => {
+  if (!posts) return null
+  if (!posts.isArray) return null
+
+  return (
+    <Container>
+      <ul className={styles.articleList}>
+        {posts.map((post) => {
+          return (
+            <li key={post.slug}>
+              <Link to={`/blog/${post.slug}`} className={styles.link}>
+                <GatsbyImage alt="" image={post.heroImage.gatsbyImageData} />
+                <h2 className={styles.title}>{post.title}</h2>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: post.description.childMarkdownRemark.html,
+                  }}
+                />
+                <div className={styles.meta}>
+                  <small className="meta">{post.publishDate}</small>
+                  <Tags tags={post.tags} />
+                </div>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </Container>
+  )
+}
 
 export default ArticlePreview
