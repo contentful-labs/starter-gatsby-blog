2597f3108354723e6c0f31241c150be9d6e76c57
Florian Kissling <florian.kissling@gmail.com>
2021-08-04 18:59:09 +0200

    Drop Bluebird/Promise in favor of async/await
    * use `reporter` to inform about errors
    * add bits of inline documentation
    * all following the official Gatsby Blog Starter (https://github.com/gatsbyjs/gatsby/blob/master/starters/blog/gatsby-node.js)

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,47 +1,53 @@
-const Promise = require('bluebird')
 const path = require('path')
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
 
-  return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/blog-post.js')
-    resolve(
-      graphql(
-        `
-          {
-            allContentfulBlogPost {
-              nodes {
-                title
-                slug
-              }
-            }
+  // Define a template for blog post
+  const blogPost = path.resolve('./src/templates/blog-post.js')
+
+  const result = await graphql(
+    `
+      {
+        allContentfulBlogPost {
+          nodes {
+            title
+            slug
           }
-        `
-      ).then((result) => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
         }
+      }
+    `
+  )
 
-        const posts = result.data.allContentfulBlogPost.nodes
-
-        posts.forEach((post, index) => {
-          const previousPostSlug = index === 0 ? null : posts[index - 1].slug
-          const nextPostSlug =
-            index === posts.length - 1 ? null : posts[index + 1].slug
-
-          createPage({
-            path: `/blog/${post.slug}/`,
-            component: blogPost,
-            context: {
-              slug: post.slug,
-              previousPostSlug,
-              nextPostSlug,
-            },
-          })
-        })
-      })
+  if (result.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your Contentful posts`,
+      result.errors
     )
-  })
+    return
+  }
+
+  const posts = result.data.allContentfulBlogPost.nodes
+
+  // Create blog posts pages
+  // But only if there's at least one blog post found in Contentful
+  // `context` is available in the template as a prop and as a variable in GraphQL
+
+  if (posts.length > 0) {
+    posts.forEach((post, index) => {
+      const previousPostSlug = index === 0 ? null : posts[index - 1].slug
+      const nextPostSlug =
+        index === posts.length - 1 ? null : posts[index + 1].slug
+
+      createPage({
+        path: `/blog/${post.slug}/`,
+        component: blogPost,
+        context: {
+          slug: post.slug,
+          previousPostSlug,
+          nextPostSlug,
+        },
+      })
+    })
+  }
 }
