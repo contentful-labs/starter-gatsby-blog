ef43da584e42a2ab6ae802fdbacb38be63afeeea
Max Hawkins <maximuskwame@gmail.com>
2021-10-05 16:03:15 -0700

    fix null tag bug

--- src/components/tags.js
+++ src/components/tags.js
@@ -3,7 +3,7 @@ import React from 'react'
 import * as styles from './tags.module.css'
 
 const Tags = ({ tags }) =>
-  tags.length > 0 && (
+  tags?.length > 0 && (
     <small className={styles.tags}>
       {tags.map((tag) => (
         <div key={tag} className={styles.tag}>
