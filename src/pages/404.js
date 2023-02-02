269dc9121a93cce2460c8c2f6352eac0c4776e00
Alex Reed <alex.reed@gatsbyjs.com>
2023-02-02 12:04:26 -0500

    404 page styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,17 @@ import Layout from '../components/layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </Layout>
 )
 
